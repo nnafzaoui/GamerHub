985ec9bfa02a09bce5733fea7606f044be8b8c2e
nnafzaoui <nafzaouinourdine@gmail.com>
2020-04-12 20:54:02 +0100

    2nd commit

--- server.js
+++ server.js
@@ -41,3 +41,25 @@ app.get('/all',sendAll);
 function sendAll(request, response) {
     response.send(words);
 }
+
+
+
+var productsData = [
+    {
+        "FirstName" : "Noureddine",
+        "LastName" : "Nafzaoui",
+        "Email" : "nafzaouinourdine@gmail.com",
+        "Product" : "PlayStation 4",
+        "ProductType" : "Console",
+        "Price" : 4000
+    },
+    {
+        "FirstName" : "Ahmed",
+        "LastName" : "Nafzaoui",
+        "Email" : "nafzaouinourdine@gmail.com",
+        "Product" : "The last of us",
+        "ProductType" : "Game",
+        "Price" : 1000
+    }
+];
+document.getElementById("app").innerHTML = `<h1>We have (${productsData.length} Products available)</h1>`
